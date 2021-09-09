dde3b8f001be673cea3333ea313ec9dd734bfede
Andrew Scribner <ca.scribner+1@gmail.com>
2021-09-09 13:53:27 -0400

    feat: migrate tests and CI to pytest-operator

---
+++ tests/integration/test_charm.py
@@ -0,0 +1,26 @@
+import logging
+from pathlib import Path
+import pytest
+import yaml
+from pytest_operator.plugin import OpsTest
+
+log = logging.getLogger(__name__)
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+APP_NAME = "admission-webhook"
+CHARM_ROOT = "."
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test: OpsTest):
+    built_charm_path = await ops_test.build_charm(CHARM_ROOT)
+    log.info(f"Built charm {built_charm_path}")
+
+    image_path = METADATA["resources"]["oci-image"]["upstream-source"]
+    resources = {"oci-image": image_path}
+
+    await ops_test.model.deploy(
+        entity_url=built_charm_path,
+        resources=resources,
+    )
+    await ops_test.model.wait_for_idle(timeout=60 * 60)
