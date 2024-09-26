74410851af0e79c33388679dedb49d4685d722b4
Orfeas Kourkakis <orfeas.kourkakis@canonical.com>
2024-09-26 16:49:06 +0300

    feat: Add Terraform module

---
+++ terraform/outputs.tf
@@ -0,0 +1,13 @@
+output "app_name" {
+  value = juju_application.admission_webhook.name
+}
+
+output "provides" {
+  value = {
+    pod_defaults = "pod-defaults",
+  }
+}
+
+output "requires" {
+  value = {}
+}
