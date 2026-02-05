5ec09bd1678146a13e0fa4fa22c72c61842027d3
NohaIhab <noha.ihab@canonical.com>
2026-02-05 18:47:51 +0200

    chore: update terraform outputs to include ambient mesh relations

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,12 +4,15 @@ output "app_name" {
 
 output "provides" {
   value = {
-    pod_defaults = "pod-defaults",
+    pod_defaults     = "pod-defaults",
+    provide_cmr_mesh = "provide-cmr-mesh"
   }
 }
 
 output "requires" {
   value = {
-    "logging" = "logging",
+    logging          = "logging",
+    require_cmr_mesh = "require-cmr-mesh",
+    service_mesh     = "service-mesh"
   }
 }
